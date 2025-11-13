d5370561c9b0882f3ffe78b88c46373f7d9c857f
sobhi2006 <sobhihazouri2006@gamil.com>
2025-11-13 11:29:06 +0300

    Init commit

---
+++ RabbitMqProject-Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await Consumer();
+    }
+    static async Task Consumer()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += async (model, ea) =>  // ea => event argument
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine("Received Message : " + message);
+            await Task.CompletedTask;
+        };
+
+        await channel.BasicConsumeAsync(queue: "q1", autoAck: true, consumer: consumer);
+        await channel.BasicConsumeAsync("q2", true, consumer);
+        await channel.BasicConsumeAsync("q3", true, consumer);
+
+        Console.WriteLine("Press enter to exist");
+        Console.ReadKey();
+    }
+}
---
+++ RabbitMqProject-Publisher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await GenerateLog();
+    }
+    static async Task GenerateLog()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(900);
+            var message = "";
+            byte[] body;
+            if (i % 2 == 0)
+            {
+                message = $"Log Info {i}";
+                body = Encoding.UTF8.GetBytes(message);
+
+                await channel.BasicPublishAsync("amq.topic", "log.info", body);
+            }
+            else
+            {
+                message = $"Log Error {i}";
+                body = Encoding.UTF8.GetBytes(message);
+
+                await channel.BasicPublishAsync("amq.topic", "log.Error", body);
+            }
+            Console.WriteLine("Send message is : " + message);
+        }
+        Console.WriteLine("Press enter to exit");
+        Console.ReadKey();
+    }
+}
